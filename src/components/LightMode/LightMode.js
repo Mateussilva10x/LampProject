3be66de3ee7d8bd1d98b3d7f07d642f99aea13d6
Mateus Silva <mateussilva651@gmail.com>
2023-05-22 14:20:08 -0300

    add the state to change the light mode to dark mode

--- src/components/LightMode/LightMode.js
+++ src/components/LightMode/LightMode.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import LampOn from "../../assets/lampOn.png";
 import LightLogo from "../../assets/lightLogo.png";
+import DarkLogo from "../../assets/darkLogo.png";
 import LightElipse from "../../assets/lightElipse.png";
 import DarkElipse from "../../assets/darkElipse.png";
 import Flash from "../../assets/flash.png";
@@ -11,21 +12,39 @@ import {
   ShoppingCart,
   Youtube,
   Twitter,
+  Mouse,
 } from "lucide-react";
 
 import "./LightMode.scss";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
 const LightMode = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChecked = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <div className="dark content">
+    <div className={!isChecked ? "light content" : "dark content"}>
       <header>
-        <img src={LightLogo} alt="logo para o modo claro" />
-        <div className="menu">
+        <img
+          src={!isChecked ? LightLogo : DarkLogo}
+          alt="logo para o modo claro"
+          className="logo"
+        />
+        <div className={!isChecked ? "menu" : "dark-mode menu"}>
           <div className="search">
-            <div className="search-icon">
+            <div
+              className={!isChecked ? "search-icon" : "search-icon dark-mode"}
+            >
               <Search size={20} />
             </div>
-            <input type="text" placeholder="search lamp..." />
+            <input
+              className={!isChecked ? "" : "dark-mode"}
+              type="text"
+              placeholder="search lamp..."
+            />
           </div>
           <ShoppingCart />
           <UserCircle />
@@ -35,26 +54,58 @@ const LightMode = () => {
       <section>
         <div className="image-container">
           <div className="elipse-container">
-            <img src={DarkElipse} alt="elipse do modo claro" />
+            <img
+              src={!isChecked ? LightElipse : DarkElipse}
+              alt="elipse do modo claro"
+            />
           </div>
           <img src={LampOn} alt="lamp" />
           <img src={Flash} alt="lamp" className="flash" />
-          <button>ON</button>
+          <ToggleSwitch onClick={handleChecked} checked={isChecked} />
           <div className="main-content">
-            <h1>change environments</h1>
-            <span>
-              All our lamps are carefully selected to ensure exceptional
-              quality, durability, and design.
+            <h1 className={!isChecked ? "" : "dark-mode"}>
+              {!isChecked ? "light your way" : "change environments"}
+            </h1>
+            <span className={!isChecked ? "" : "dark-mode"}>
+              {!isChecked
+                ? "We offer a wide range of fixtures to help you light your space efficiently and elegantly. From modern pendants to classic floor lamps, we have options for every style and need."
+                : "All our lamps are carefully selected to ensure exceptional quality, durability, and design."}
             </span>
           </div>
           <div className="footer">
-            <p>follow us to get the news</p>
+            <div className={!isChecked ? "list-menu" : "dark-mode list-menu"}>
+              <ul>
+                <li>home</li>
+                <li>products</li>
+                <li>projects</li>
+                <li>sale</li>
+                <li>help</li>
+              </ul>
+            </div>
+
+            <div
+              className={
+                !isChecked
+                  ? "footer-icon-center"
+                  : "footer-icon-center dark-mode"
+              }
+            >
+              <Mouse />
+              <ChevronDown />
+            </div>
             <div className="social-medias">
-              <div className="media-icon">
-                <Youtube />
-              </div>
-              <div className="media-icon">
-                <Twitter />
+              <p>follow us to get the news</p>
+              <div className="social-medias-icons">
+                <div
+                  className={!isChecked ? "media-icon" : "dark-mode media-icon"}
+                >
+                  <Youtube />
+                </div>
+                <div
+                  className={!isChecked ? "media-icon" : "dark-mode media-icon"}
+                >
+                  <Twitter />
+                </div>
               </div>
             </div>
           </div>
